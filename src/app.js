3c5af7a33d11efd6702963ab89d846023a247f2c
AliRahimi123456 <162766248+AliRahimi123456@users.noreply.github.com>
2024-04-06 02:57:08 +0000

    completed the domain-Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adjective = ["Great", "big"];
+  let noun = ["Jogger", "raccon"];
+  let rul = [".com", ".net", ".dev"];
+
+  console.log("Regular for loop:");
+
+  for (let pronounIndex = 0; pronounIndex < pronoun.length; ++pronounIndex) {
+    for (let adjIndex = 0; adjIndex < adjective.length; ++adjIndex) {
+      for (let nounIndex = 0; nounIndex < noun.length; ++nounIndex) {
+        for (let rulIndex = 0; rulIndex < rul.length; ++rulIndex) {
+          console.log(
+            `${pronoun[pronounIndex]}${adjective[adjIndex]}${noun[nounIndex]}${rul[rulIndex]}`
+          );
+        }
+      }
+    }
+  }
+
+  let domainGen = document.querySelector(".domain-gen");
+
+  for (let pronounIndex = 0; pronounIndex < pronoun.length; ++pronounIndex) {
+    for (let adjIndex = 0; adjIndex < adjective.length; ++adjIndex) {
+      for (let nounIndex = 0; nounIndex < noun.length; ++nounIndex) {
+        for (let rulIndex = 0; rulIndex < rul.length; ++rulIndex) {
+          let domainName = `${pronoun[pronounIndex]}${adjective[adjIndex]}${noun[nounIndex]}${rul[rulIndex]}`;
+          console.log(domainName);
+        }
+      }
+    }
+  }
 };
